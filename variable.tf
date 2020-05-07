cd3d0e1f00fdefc4fbcbd3fcc747a576f47aad53
mo <mkarimidevops@gmail.com>
2020-05-07 00:34:16 -0500

    cidr range changed

--- variable.tf
+++ variable.tf
@@ -1,10 +1,10 @@
 variable "vpc1_cidr" {
-    default = "198.0.0.0/16"
+    default = "192.0.0.0/16"
 }
 
 variable "pub_sub_cidr1" {
     type = "list"
-    default = ["198.1.0.0/24","198.2.0.0/24","198.3.0.0/24","198.4.0.0/24"]  
+    default = ["192.1.0.0/24","192.2.0.0/24","192.3.0.0/24","192.4.0.0/24"]  
 }
 variable "azs_vpc1" {
     type = "list"
