a811b1246c4db66aa0e0db141cbd2ba6ee767b4d
mo <mkarimidevops@gmail.com>
2020-05-07 00:44:31 -0500

    Changed

--- variable.tf
+++ variable.tf
@@ -1,10 +1,10 @@
 variable "vpc1_cidr" {
-    default = "172.0.0.0/16"
+    default = "192.168.0.0/16"
 }
 
 variable "pub_sub_cidr1" {
     type = "list"
-    default = ["172.1.0.0/24","172.2.0.0/24","172.3.0.0/24","172.4.0.0/24"]  
+    default = ["192.168.1.0/24","192.168.2.0/24","192.168.3.0/24","192.168.4.0/24"]  
 }
 variable "azs_vpc1" {
     type = "list"
