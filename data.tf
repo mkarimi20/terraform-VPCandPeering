b2c07bae2df30d305db0014c969ad556241193de
mo <mkarimidevops@gmail.com>
2020-05-07 00:27:10 -0500

    changed

--- data.tf
+++ data.tf
@@ -1,1 +1,1 @@
-data  "aws_availalibility_zones" "azs" {}
+data  "aws_availability_zones" "azs" {}
--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "pub_subnets_vpc1" {
-    count    = "${length(data.aws_availalibility_zones.azs.names)}"
+    count    = "${length(data.aws_availability_zones.azs.names)}"
   vpc_id     = "${aws_vpc.vpc1.id}"
-  availability_zone = "${element(data.aws_availalibility_zones.azs.names,count.index)}"
+  availability_zone = "${element(data.aws_availability_zones.azs.names,count.index)}"
   cidr_block = "${element(var.pub_sub_cidr1,count.index)}"
   
 
