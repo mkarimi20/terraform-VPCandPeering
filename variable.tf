eb35c9d0bf376d10bedf8ed3a9e917b51134b7fa
mo <mkarimidevops@gmail.com>
2020-05-07 00:25:15 -0500

    cahnged

--- variable.tf
+++ variable.tf
@@ -6,10 +6,6 @@ variable "pub_sub_cidr1" {
     type = "list"
     default = ["10.1.0.0/24","10.2.0.0/24","10.3.0.0/24"]  
 }
-
-variable "name" {
-  
-}
 variable "azs_vpc1" {
     type = "list"
     default = ["us-west-2a","us-west-2b","us-west-2c",]
